d9b18b5bfd7b07eb22e80f813d60cda5d863c397
ford@AcerS1 <forgacs.daniel@gmail.com>
2018-11-14 12:37:52 +0000

    added docs

--- MazeSolverMichaelShell.py
+++ MazeSolverMichaelShell.py
@@ -1,4 +1,14 @@
 """
+this is the guide to help with orientattion
+and find what's left and right is.
+Lef and right is relative to the orientation,
+so it's important to know which direction
+we're moving. If you're moving north,
+left is east. But if you're moving
+south, left is west.
+
+This little map is how the "tiny" maze
+looks like.
         y
 
     ---X------
@@ -28,6 +38,9 @@ Image.MAX_IMAGE_PIXELS = 1000000000
 import time
 
 
+# I added these guys to make sure I don't get into
+# an infinite loop if I make a mistake while I'm developing the code.
+# it's usualy a good idea using "while" loops
 MAXITER = 1000
 CURRENTITER = 0
 
@@ -48,11 +61,20 @@ t0 = time.time()
 im = Image.open(image)
 width, height = im.size[0], im.size[1]
 
+# this is the list where your boss stores
+# the pixel values of the image.
+# 1 is white, 0 is black.
 data = list(im.getdata(0))
 
 listMap = []
 rowList = []
 rowCount = 0
+
+# here he breakes down the pixel values into a
+# two dimensional list, so we can check the pixel
+# values as coordinates. The "listmap" variable
+# looks like the image. listmap[0][0] is the
+# top left corner pixe of the image
 for i in data:
     rowCount = len(rowList)
 
@@ -69,6 +91,11 @@ for i in data:
 
 idx = 0
 
+
+# here he just finds where the first and the last
+# steps are. he scans the first and the last rows
+# of the image, and if it's white, he assumes
+# that's the start and the end position.
 for i in listMap[0]:
     if i == 1:
         startPos = idx
@@ -97,16 +124,33 @@ print("Start Time: ", t0)
 Use this space for helper functions and declaring varibales
 """
 
+# I'm initializing some values. like the startPos
+# and endPos are single integers. I turn them to coordinates
 initcoord = (0, startPos)
 endcoord = (height-1, endPos)
+
+# I initialize where we start the maze
 currentPos = initcoord
+
+# and set the initial orientation to south.
 orientation = 'south'
+
+# add the first step coordinate to the solution list
 resultpath = (initcoord,)
 is_leftwall = None
 is_frontwall = None
 
 
 def check_left_wall(pos, orientation):
+    """
+    given the current coordinate we're at
+    and the orientation this function
+    checks if there's a wall to the left
+    and returns true or false.
+
+    this is the reason why we have to know
+    where we're heading as the orientation.
+    """
     if orientation == 'north':
         leftcoord = pos[0], pos[1]-1
     elif orientation == 'south':
@@ -117,6 +161,14 @@ def check_left_wall(pos, orientation):
     elif orientation == 'west':
         leftcoord = pos[0]-1, pos[1]
 
+    # the next part is an ugly one. if we're on the edge
+    # of the maze checking if there's a wall to the left
+    # will error, because the coordinate we're checking
+    # is outside of the maze, so the coordinate doesn't
+    # exist in the list, so we get an error. This should
+    # be written much nicer, but this get's the job done.
+    # if the coordinate errors when I check it, the code
+    # assumes there's a wall
     try:
         result = not bool(listMap[leftcoord[0]][leftcoord[1]])
     except:
@@ -127,6 +179,10 @@ def check_left_wall(pos, orientation):
 
 
 def check_front_wall(pos, orientation):
+    """
+    this is the same as the left wall function.
+    Just check different coordinates.
+    """
     if orientation == 'north':
         frontcoord = pos[0]-1, pos[1]
     elif orientation == 'south':
@@ -146,6 +202,16 @@ def check_front_wall(pos, orientation):
 
 
 def turn(orientation, clockwise=True):
+    """
+    this function turns. meaning it changes
+    orientation. I only wrote it to turn in
+    one direction. If the code needs to turn
+    to the other side, it simple turns
+    3 times. That makes it simple :)
+    this simple sets our new orientation
+    based on our current orientation
+    and returns it
+    """
     repeat = range(3) if clockwise else range(1)
 
     for turn in repeat:
@@ -164,6 +230,18 @@ def turn(orientation, clockwise=True):
 
 
 def move(pos, orientation):
+    """
+    given a position coordinate
+    and our current orientation,
+    this guy calculates a step forward
+    and returne the new coordinate.
+
+    this is the other reason we need to store and know
+    our current orientation. Moving forward one step
+    means different things if you're facing
+    north and another thing when you're
+    facing south.
+    """
     # if pos[0] < height-1:
     #     newpos = pos[0]+1, pos[1]
     # else:
@@ -181,12 +259,17 @@ def move(pos, orientation):
 
 
 
+# This is the main event loop where everything is happening.
+# this is the part where the algorythm we found online
+# is happening
 while True:
 
     """
     Michael put your movement code here
 
     """
+    # I start by checking the walls and store this info
+    # so I can make the decisions later
     is_leftwall = check_left_wall(
         pos=currentPos, orientation=orientation)
     is_frontwall = check_front_wall(
@@ -198,26 +281,40 @@ while True:
     print('--> is wall to front:', is_frontwall)
 
     # Dont for get to change this to something that sets completed to True
+
+    # this part checks if the coordinate of our current
+    # position is the end position. if it is, we found our way
+    # out and can terminate the main event loop
     if currentPos == endcoord:
         break
         print("Found The Exit")
 
 
+    # here I check if there's a wall to the left or not.
     if not is_leftwall:
+        # if there's no wall then turn, move and store the coordinates
         orientation = turn(orientation=orientation)
         currentPos = move(pos=currentPos, orientation=orientation)
         resultpath = resultpath + (currentPos,)
 
     else:
+        # this is what's happening if there's a wall to the left
         if not is_frontwall:
+            # if there's no wall in front of us, we move without turning
+            # and store our new position
             currentPos = move(pos=currentPos, orientation=orientation)
             resultpath = resultpath + (currentPos,)
 
         else:
+            # this is what's happening if there's a wall to the left
+            # and there's a wall in front of us. we simple turn
+            # without moving and go the the next loop
             orientation = turn(orientation=orientation, clockwise=False)
 
 
 
+    # this is just a safe guard counter to avoid infinite loops
+    # if the code can't find the end position
     CURRENTITER += 1
 
     if CURRENTITER == MAXITER:
@@ -250,6 +347,11 @@ print ("Saving Image")
 im = im.convert('RGB')
 impixels = im.load()
 
+
+# in this part here your boss just takes the collected list
+# of steps we made, and set's pixel colour for a new image
+# so he can save the solution to a new image so you can see
+# what appened.
 for i in range(0, length - 1):
     a = resultpath[i]
     b = resultpath[i+1]
